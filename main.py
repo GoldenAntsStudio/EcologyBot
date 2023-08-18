f36e1afe7be527bc763c8ec681b8c37ccd9b87af
Smart Ant Studio <hometikhon777@gmail.com>
2023-08-18 19:37:52 +0300

    1.2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webbrowser
 import os, random
 from discord.ext import commands
 
+version = 1.2
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,8 +22,12 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 @bot.command()
-async def helpme(ctx):
+async def help(ctx):
     await ctx.send('/mem -- мемы; /info материал -- информация')
+
+@bot.command()
+async def version(ctx):
+    await ctx.send(f'Версия бота: {version}')
 
 @bot.command()
 async def mem(ctx):
